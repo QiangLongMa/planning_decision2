8484ecc5a63a8c5551db4fc62ecb33ea83a17e0f
RIngKing1 <1480333253@qq.com>
2025-02-21 13:37:48 +0800

    测试_lanefollow

--- src/Planning/src/senario/lane_follow.cpp
+++ src/Planning/src/senario/lane_follow.cpp
@@ -8,30 +8,29 @@
 LaneFollowScenario::LaneFollowScenario(const Eigen::VectorXd &car, const Eigen::MatrixXd &globalPath,
                                        const std::vector<obses_sd> &obses_limit_SD,
                                        const std::vector<Eigen::VectorXd> &GlobalcoordinatesystemObsesLimit,
-                                       const double &gpsA, const double indexinglobalpath)  
+                                       const double &gpsA, const double indexinglobalpath)
     : Scenario(car, globalPath, obses_limit_SD, GlobalcoordinatesystemObsesLimit, gpsA, indexinglobalpath)
 {
-
-
-
-
-
 }
 
-bool LaneFollowScenario::Straight() {
-    start_l = -1.5; end_l = -1.5; target_l = -1.5; 
-    delta_l = 0.5; target_v = 10;
+bool LaneFollowScenario::Straight()
+{
+    start_l = -1.5;
+    end_l = -1.5;
+    target_l = -1.5;
+    delta_l = 0.5;
+    target_v = 10;
     frentPoint FrentPoint_;
     int car_index;
     senarioTools::findClosestPoint(car_(0), car_(1), optTrajxy, car_index);
     senarioTools::cartofrenet(car_, optTrajxy, car_index, FrentPoint_);
-     Eigen::VectorXd vehicle_state_(6);
-    vehicle_state_ << car_(0), car_(1),car_(2),car_(3),car_(4), gpsA_;
+    Eigen::VectorXd vehicle_state_(6);
+    vehicle_state_ << car_(0), car_(1), car_(2), car_(3), car_(4), gpsA_;
     std::array<double, 6> vehicle_state = senarioTools::Decidestartsl(FrentPoint_, car_index, indexinglobalpath_,
-                                        optTrajxy, globalPath, vehicle_state_, optTrajsd);
+                                                                      optTrajxy, globalPath, vehicle_state_, optTrajsd);
     local_start_s = vehicle_state[0];
     local_start_l = vehicle_state[1];
-    dl = vehicle_state[2]; 
+    dl = vehicle_state[2];
     ddl = vehicle_state[3];
     speed = vehicle_state[4];
     gpsA_ = vehicle_state[5];
@@ -39,27 +38,32 @@ bool LaneFollowScenario::Straight() {
     LOCAL_.setPatam(gpsA_, speed, FrentPoint_.s, FrentPoint_.d, dl, ddl, globalPath, 30, 10, car_index, obses_limit_SD, GlobalcoordinatesystemObsesLimit,
                     start_l, end_l, delta_l, target_v, target_l, Decisionflags_, 0, true, false, 0, 0); // 最后一位时最近障碍物的位置
     flag = LOCAL_.GetoptTrajxy(lastOptTrajxy, lastOptTrajsd);
-    if (flag) {
+    if (flag)
+    {
         UpdateLocalPath();
     }
     return flag;
 }
 
 // 避障
-bool LaneFollowScenario::AvoidObstacle() {
-    start_l = -0.5; end_l = -2.5; target_l = -1.5; 
-    delta_l = 0.5; target_v = 10;
+bool LaneFollowScenario::AvoidObstacle()
+{
+    start_l = -0.5;
+    end_l = -2.5;
+    target_l = -1.5;
+    delta_l = 0.5;
+    target_v = 10;
     frentPoint FrentPoint_;
     int car_index;
     senarioTools::findClosestPoint(car_(0), car_(1), optTrajxy, car_index);
     senarioTools::cartofrenet(car_, optTrajxy, car_index, FrentPoint_);
-     Eigen::VectorXd vehicle_state_(6);
-    vehicle_state_ << car_(0), car_(1),car_(2),car_(3),car_(4), gpsA_;
+    Eigen::VectorXd vehicle_state_(6);
+    vehicle_state_ << car_(0), car_(1), car_(2), car_(3), car_(4), gpsA_;
     std::array<double, 6> vehicle_state = senarioTools::Decidestartsl(FrentPoint_, car_index, indexinglobalpath_,
-                                        optTrajxy, globalPath, vehicle_state_, optTrajsd);
+                                                                      optTrajxy, globalPath, vehicle_state_, optTrajsd);
     local_start_s = vehicle_state[0];
     local_start_l = vehicle_state[1];
-    dl = vehicle_state[2]; 
+    dl = vehicle_state[2];
     ddl = vehicle_state[3];
     speed = vehicle_state[4];
     gpsA_ = vehicle_state[5];
@@ -67,7 +71,8 @@ bool LaneFollowScenario::AvoidObstacle() {
     LOCAL_.setPatam(gpsA_, speed, FrentPoint_.s, FrentPoint_.d, dl, ddl, globalPath, 30, 10, car_index, obses_limit_SD, GlobalcoordinatesystemObsesLimit,
                     start_l, end_l, delta_l, target_v, target_l, Decisionflags_, 0, true, false, 0, 0);
     flag = LOCAL_.GetoptTrajxy(lastOptTrajxy, lastOptTrajsd);
-    if (flag) {
+    if (flag)
+    {
         UpdateLocalPath();
     }
     return flag;
@@ -76,54 +81,67 @@ void LaneFollowScenario::Overtake()
 {
 }
 
-//减速停车 目前的逻辑是先获取直线行使的局部路径 然后使用获取的路径与当前的障碍物进行碰撞，
-//获取与自车发生碰撞的s， 之后减速停车 如果s较小 则触发AEB
-bool LaneFollowScenario::DecelerateFollow() {
-    //创建两个空的局部路径生成的障碍物信息，目的为了通过里面的障碍物检测模块
+// 减速停车 目前的逻辑是先获取直线行使的局部路径 然后使用获取的路径与当前的障碍物进行碰撞，
+// 获取与自车发生碰撞的s， 之后减速停车 如果s较小 则触发AEB
+bool LaneFollowScenario::DecelerateFollow()
+{
+    // 创建两个空的局部路径生成的障碍物信息，目的为了通过里面的障碍物检测模块
     std::vector<obses_sd> temporary_obses_limit_SD;
     std::vector<Eigen::VectorXd> temporary_GlobalcoordinatesystemObsesLimit;
-    start_l = -0.5; end_l = -2.5; target_l = -1.5; 
-    delta_l = 0.5; target_v = 10;
+    start_l = -0.5;
+    end_l = -2.5;
+    target_l = -1.5;
+    delta_l = 0.5;
+    target_v = 10;
     frentPoint FrentPoint_;
     int car_index;
     senarioTools::findClosestPoint(car_(0), car_(1), optTrajxy, car_index);
     senarioTools::cartofrenet(car_, optTrajxy, car_index, FrentPoint_);
-     Eigen::VectorXd vehicle_state_(6);
-    vehicle_state_ << car_(0), car_(1),car_(2),car_(3),car_(4), gpsA_;
+    Eigen::VectorXd vehicle_state_(6);
+    vehicle_state_ << car_(0), car_(1), car_(2), car_(3), car_(4), gpsA_;
     std::array<double, 6> vehicle_state = senarioTools::Decidestartsl(FrentPoint_, car_index, indexinglobalpath_,
-                                        optTrajxy, globalPath, vehicle_state_, optTrajsd);
+                                                                      optTrajxy, globalPath, vehicle_state_, optTrajsd);
     local_start_s = vehicle_state[0];
     local_start_l = vehicle_state[1];
-    dl = vehicle_state[2]; 
+    dl = vehicle_state[2];
     ddl = vehicle_state[3];
     speed = vehicle_state[4];
     gpsA_ = vehicle_state[5];
     RestFlags(true, false, false, false, false);
     LOCAL_.setPatam(gpsA_, speed, FrentPoint_.s, FrentPoint_.d, dl, ddl, globalPath, 30, 10, car_index, obses_limit_SD, GlobalcoordinatesystemObsesLimit,
                     start_l, end_l, delta_l, target_v, target_l, Decisionflags_, 0, true, false, 0, 0);
-    if (!LOCAL_.GetoptTrajxy(lastOptTrajxy, lastOptTrajsd)) {
+    if (!LOCAL_.GetoptTrajxy(lastOptTrajxy, lastOptTrajsd))
+    {
         return false;
-    } 
-    //若获取当前的行使路径
+    }
+    // 若获取当前的行使路径
     std::pair<bool, double> CollisionAndS;
     CollisionAndS = senarioTools::Pathplanningduringdeceleration(lastOptTrajxy, GlobalcoordinatesystemObsesLimit);
-    if (!CollisionAndS.first) { //如果当前路径没有碰撞 
+    if (!CollisionAndS.first)
+    { // 如果当前路径没有碰撞
         UpdateLocalPath();
-        return true; 
+        return true;
     }
-    if (CollisionAndS.second > distance_threshold) {
-        start_l = -1.0; end_l = -2; target_l = -1.5; 
-        delta_l = 0.5; target_v = 0;
-        int LengthLocalPath = CollisionAndS.second - distance_threshold ; //局部路径的长度 
-        RestFlags(false, false, true, false, false);//设置标志符
+    if (CollisionAndS.second > distance_threshold)
+    {
+        start_l = -1.0;
+        end_l = -2;
+        target_l = -1.5;
+        delta_l = 0.5;
+        target_v = 0;
+        int LengthLocalPath = CollisionAndS.second - distance_threshold; // 局部路径的长度
+        RestFlags(false, false, true, false, false);                     // 设置标志符
         LOCAL_.setPatam(gpsA_, speed, FrentPoint_.s, FrentPoint_.d, dl, ddl, globalPath, LengthLocalPath, LengthLocalPath, car_index, obses_limit_SD, GlobalcoordinatesystemObsesLimit,
-                    start_l, end_l, delta_l, target_v, target_l, Decisionflags_, 0, false, false, 0, 0);
+                        start_l, end_l, delta_l, target_v, target_l, Decisionflags_, 0, false, false, 0, 0);
         flag = LOCAL_.GetoptTrajxy(lastOptTrajxy, lastOptTrajsd);
-        if (flag) {
+        if (flag)
+        {
             UpdateLocalPath();
         }
         return flag;
-    } else {
+    }
+    else
+    {
         /**********AEB************/
     }
 }
@@ -135,12 +153,22 @@ void LaneFollowScenario::ReturnRightLane()
 void LaneFollowScenario::MakeDecision()
 {
     // 判断迭代容器vector obses_limit_SD 是否为空
-  
+    RestFlags(true, false, false, false, false);
+    // 测试给出直行决策
 }
-
-
 
 bool LaneFollowScenario::Process()
 {
-    return false;
+    if (Decisionflags_.DriveStraightLineFlag)
+        Straight();
+
+    /****************************判断是否找到路径******************************************* */
+    if (find_local_path_)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
